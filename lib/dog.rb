9a86cad8d3cfd9895936e419df1bd10bd6418cc3
Vince Andrew Pimentel <contact@vincep.me>
2018-12-22 03:16:17 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -52,4 +52,13 @@ class Dog
     dog
   end
 
+  def update
+    sql = <<-SQL
+      UPDATE dogs
+      SET name = ?, breed = ?
+      WHERE id = ?;
+    SQL
+
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
+  end
 end

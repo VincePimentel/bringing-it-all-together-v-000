b7db625897d401c335ff50506672aa1e142bc57e
Vince Andrew Pimentel <contact@vincep.me>
2018-12-22 03:40:38 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -29,7 +29,7 @@ class Dog
   end
 
   def self.new_from_db(row)
-    dog = self.new(row[1], row[2], row[0])
+    dog = self.new(name: row[1], breed: row[2], id: row[0])
     dog
   end
 
@@ -101,9 +101,5 @@ class Dog
     SQL
 
     DB[:conn].execute(sql, self.name, self.breed, self.id)
-  end
-
-  def self.new_from_db
-
   end
 end

7efe7c1af72a30f5cc5f0710a2e9698cac5d2bce
Vince Andrew Pimentel <contact@vincep.me>
2018-12-22 02:48:57 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,10 +2,10 @@ class Dog
   attr_accessor :name, :breed
   attr_reader :id
 
-  def initialize(info = {name, breed, id = nil})
-    @id = info[:id]
-    @name = info[:name]
-    @breed = info[:breed]
+  def initialize(name:, breed:, id: nil)
+    @id = id
+    @name = name
+    @breed = breed
   end
 
 end
